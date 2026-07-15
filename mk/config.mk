24f69e13d68d13209da3d3ff213777469f85bb0b
Felix Jacobi <felix.jacobi@iserv.eu>
2026-07-15 12:01:33 +0200

    Add app/vite.config.ts to ASSETS_SRC in config.mk to trigger asset rebuilds on config change

--- mk/config.mk
+++ mk/config.mk
@@ -5,7 +5,7 @@ BIN_WEBPACK=npm
 WEBPACK_FLAGS=run build:iservmake --
 WEBPACK_INTERNAL_ARGS=
 ASSETS_MANIFEST=app/public/static/.vite/manifest.json
-ASSETS_SRC=$(shell find app/assets -type f)
+ASSETS_SRC=app/vite.config.ts $(shell find app/assets -type f)
 SYMFONY_USER=iserv-unificonnector
 
 LOCALE_DISABLE_POOTLE_DOWNLOAD=1
