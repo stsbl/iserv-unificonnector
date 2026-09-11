bf167984b69ab4710814fbc0925ec96b329bd7a1
Felix Jacobi <felix.jacobi@iserv.eu>
2026-09-11 15:32:04 +0200

    fix: rename active legacy UniFi user

--- iservchk/10system/35unificonnector_rename.sh
+++ iservchk/10system/35unificonnector_rename.sh
@@ -4,7 +4,8 @@ cat <<'EOT'
 Test "migrate iserv-unificonnector system user"
   ! getent passwd iserv-unificonnector || getent passwd stsbl-iserv-unificonnector
   ---
-  groupmod -n stsbl-iserv-unificonnector iserv-unificonnector
-  usermod -l stsbl-iserv-unificonnector -g stsbl-iserv-unificonnector iserv-unificonnector
+  usermod --prefix / --login stsbl-iserv-unificonnector iserv-unificonnector
+  groupmod --prefix / --new-name stsbl-iserv-unificonnector iserv-unificonnector
+  usermod --prefix / --gid stsbl-iserv-unificonnector stsbl-iserv-unificonnector
 
 EOT
